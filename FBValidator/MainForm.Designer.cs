3a7363c5a84c55a18385857ad017d5827d2863a8
evpobr <evpobr@users.noreply.github.com>
2017-05-31 16:40:55 +0500

    Release v0.1

--- FBValidator/MainForm.Designer.cs
+++ FBValidator/MainForm.Designer.cs
@@ -29,11 +29,12 @@
         private void InitializeComponent()
         {
             this.labelInput = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBoxInput = new System.Windows.Forms.TextBox();
             this.buttonOpen = new System.Windows.Forms.Button();
             this.buttonValidate = new System.Windows.Forms.Button();
             this.labelInformation = new System.Windows.Forms.Label();
             this.textBoxInformation = new System.Windows.Forms.TextBox();
+            this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
             this.SuspendLayout();
             // 
             // labelInput
@@ -45,30 +46,36 @@
             this.labelInput.TabIndex = 0;
             this.labelInput.Text = "Select FictionBook to validate:";
             // 
-            // textBox1
+            // textBoxInput
             // 
-            this.textBox1.Location = new System.Drawing.Point(15, 25);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(643, 20);
-            this.textBox1.TabIndex = 1;
+            this.textBoxInput.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBoxInput.Location = new System.Drawing.Point(15, 25);
+            this.textBoxInput.Name = "textBoxInput";
+            this.textBoxInput.Size = new System.Drawing.Size(643, 20);
+            this.textBoxInput.TabIndex = 1;
             // 
             // buttonOpen
             // 
+            this.buttonOpen.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.buttonOpen.Location = new System.Drawing.Point(664, 22);
             this.buttonOpen.Name = "buttonOpen";
             this.buttonOpen.Size = new System.Drawing.Size(75, 23);
             this.buttonOpen.TabIndex = 2;
             this.buttonOpen.Text = "Open...";
             this.buttonOpen.UseVisualStyleBackColor = true;
+            this.buttonOpen.Click += new System.EventHandler(this.buttonOpen_Click);
             // 
             // buttonValidate
             // 
+            this.buttonValidate.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.buttonValidate.Location = new System.Drawing.Point(745, 22);
             this.buttonValidate.Name = "buttonValidate";
             this.buttonValidate.Size = new System.Drawing.Size(75, 23);
             this.buttonValidate.TabIndex = 3;
             this.buttonValidate.Text = "Validate";
             this.buttonValidate.UseVisualStyleBackColor = true;
+            this.buttonValidate.Click += new System.EventHandler(this.buttonValidate_Click);
             // 
             // labelInformation
             // 
@@ -81,11 +88,18 @@
             // 
             // textBoxInformation
             // 
+            this.textBoxInformation.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.textBoxInformation.Location = new System.Drawing.Point(15, 73);
             this.textBoxInformation.Multiline = true;
             this.textBoxInformation.Name = "textBoxInformation";
             this.textBoxInformation.Size = new System.Drawing.Size(805, 297);
             this.textBoxInformation.TabIndex = 5;
+            // 
+            // openFileDialog
+            // 
+            this.openFileDialog.Filter = "FictionBook documents|*.fb2|All files|*.*";
             // 
             // MainForm
             // 
@@ -96,7 +110,7 @@
             this.Controls.Add(this.labelInformation);
             this.Controls.Add(this.buttonValidate);
             this.Controls.Add(this.buttonOpen);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.textBoxInput);
             this.Controls.Add(this.labelInput);
             this.Name = "MainForm";
             this.Text = "FBValidator";
@@ -108,11 +122,12 @@
         #endregion
 
         private System.Windows.Forms.Label labelInput;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBoxInput;
         private System.Windows.Forms.Button buttonOpen;
         private System.Windows.Forms.Button buttonValidate;
         private System.Windows.Forms.Label labelInformation;
         private System.Windows.Forms.TextBox textBoxInformation;
+        private System.Windows.Forms.OpenFileDialog openFileDialog;
     }
 }
 
--- FBValidator/MainForm.cs
+++ FBValidator/MainForm.cs
@@ -7,14 +7,48 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml;
+using System.Xml.Linq;
+using System.Xml.Schema;
 
 namespace FBValidator
 {
     public partial class MainForm : Form
     {
+        readonly XmlSchema FB2Schema;
+        static readonly List<string> statusLines = new List<string>();
+
         public MainForm()
         {
             InitializeComponent();
+
+            using (var FB2SchemaReader = XmlReader.Create(@"Schemas\FB2\2.2\FictionBook.xsd"))
+            {
+                FB2Schema = XmlSchema.Read(FB2SchemaReader, null);
+            }
+
+        }
+
+        static void FB2ValidationEventHandler(object sender, ValidationEventArgs e)
+        {
+            statusLines.Add(e.Message);
+        }
+
+        private void buttonValidate_Click(object sender, EventArgs e)
+        {
+
+            var FB2Document = new XmlDocument();
+            FB2Document.Schemas.Add(@"http://www.gribuser.ru/xml/fictionbook/2.0", @"Schemas\FB2\2.1\FictionBook.xsd");
+            FB2Document.Load(textBoxInput.Text);
+            FB2Document.Validate(FB2ValidationEventHandler);
+
+            textBoxInformation.Lines = statusLines.ToArray();
+        }
+
+        private void buttonOpen_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+                textBoxInput.Text = openFileDialog.FileName;
         }
     }
 }
